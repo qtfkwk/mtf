5b9a21b1851116123918e3184ec9348987299e95
qtfkwk <qtfkwk+mtf@gmail.com>
2025-05-10 11:34:41 -0400

    0.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,5 @@
 
 * 0.1.0 (2025-04-26): Initial release
 * 0.2.0 (2025-04-26): Add CLI example
+* 0.3.0 (2025-05-10): Read from stdin if no input files or input file is `-`; update dependencies
 
--- src/main.rs
+++ src/main.rs
@@ -25,15 +25,28 @@ struct Args {
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    for input_file in &args.input_files {
-        match std::fs::read_to_string(input_file) {
-            Ok(input) => {
-                print!("{}", process(&input)?);
-            }
-            Err(e) => {
-                return Err(anyhow!("Could not read `{}` ({e})", input_file.display()));
+    if args.input_files.is_empty() {
+        process_stdin()?;
+    } else {
+        for input_file in &args.input_files {
+            if input_file.display().to_string() == "-" {
+                process_stdin()?;
+            } else {
+                match std::fs::read_to_string(input_file) {
+                    Ok(input) => {
+                        print!("{}", process(&input)?);
+                    }
+                    Err(e) => {
+                        return Err(anyhow!("Could not read `{}` ({e})", input_file.display()));
+                    }
+                }
             }
         }
     }
+    Ok(())
+}
+
+fn process_stdin() -> Result<()> {
+    print!("{}", process(&std::io::read_to_string(std::io::stdin())?)?);
     Ok(())
 }
